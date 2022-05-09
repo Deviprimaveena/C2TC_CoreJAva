57ed0fb8151b25d29518d6de05f92655ed42fb9c
admin <admin@DESKTOP-4IJK4EI>
2022-05-09 11:49:38 -0700

    anagram,even r odd, palindrome

---
+++ EvenOrOdd/src/pgms/EvenOrOdd.java
@@ -0,0 +1,12 @@
+package pgms;
+
+public class EvenOrOdd {
+	public static void main(String [] args)
+	{
+		int n = 35;
+		if(n % 2 == 0)
+         System.out.println("The number is even");	
+		else
+			System.out.println("The number is odd");
+	}
+}
